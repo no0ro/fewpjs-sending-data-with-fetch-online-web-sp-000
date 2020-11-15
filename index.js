5c51a72032697040324235293f08ac3e98224763
no0ro <nrolwes@gmail.com>
2020-11-15 13:51:12 -0500

    commit tree 7e35394cd5f7f12d443aa4cdf024cb24ad09d9dd

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+
+
+function submitData(username, useremail){
+    
+    return fetch('http://localhost:3000/users', {
+        method: "Post",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: username,
+            email: useremail
+        })
+    })
+    .then(function(response){
+        return response.json() // convert [Response][response] to an object
+    })
+    .then(function(object){
+        document.body.innerHTML = object['id'] // impt to use innerHTML here to get test to pass
+    })
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    })
+    
+}
